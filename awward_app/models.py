ec26104c58813ccf08f730bd387ac77d55a49c59
FGacheru <francisgacheru2001@gmail.com>
2021-01-24 01:57:58 +0300

    create profile class which creates and saves profile

--- awward_app/models.py
+++ awward_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
 
 # Create your models here.
+class Profile(models.Model):
+    Profile = models.ImageField(upload_to='profile/')
+    bio = models.TextField(blank=True)
+    user_id = models.OneToOneField(User, null= True, on_delete=models.CASCADE)
+    more_info = models.TextField(blank=False, null=True)
+    
+    def __str__(self)
+        return self.bio
+    
+    def save_user(self)
+        self.save()
+     
+    @receiver(post_save, sender=User)  
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user_id=instance)
+            
+    @receiver(post_save, sender=User)
+    def save_profile(sender, instance, **kwargs)
+    isinstance.profile.save()
+    
