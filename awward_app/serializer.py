c0d123f095f8c2aba8ddd2853a5d895f268cf9f4
FGacheru <francisgacheru2001@gmail.com>
2021-01-24 13:15:59 +0300

    add ProjectsSerializer and ProfileSerializer

---
+++ awward_app/serializer.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import *
+
+class ProjectsSerialize(serializers.ModelSerializer):
+    class Meta:
+        model = Projects
+        fields = ('id', 'project_name', 'image', 'description', 'pub_date', 'author', 'author_photo', 'url')
+        
+        
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields =('id','profile', 'bio', 'user_id', 'more_info')
