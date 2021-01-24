0494a196e4ee0f237efb27811e4bd1dc9da0aa95
FGacheru <francisgacheru2001@gmail.com>
2021-01-24 16:43:21 +0300

    import CloudinaryField from  cloudinary.models

--- awward_app/models.py
+++ awward_app/models.py
@@ -12,20 +12,13 @@ class Profile(models.Model):
     user_id = models.OneToOneField(User, null= True, on_delete=models.CASCADE)
     more_info = models.TextField(blank=False, null=True)
     
-    def __str__(self)
+    def __str__(self):
         return self.bio
     
-    def save_user(self)
+    def save_user(self):
         self.save()
      
-    @receiver(post_save, sender=User)  
-    def create_profile(sender, instance, created, **kwargs):
-        if created:
-            Profile.objects.create(user_id=instance)
-            
-    @receiver(post_save, sender=User)
-    def save_profile(sender, instance, **kwargs)
-    isinstance.profile.save()
+    
     
 class Projects(models.Model):
     project_name = models.CharField(max_length=300)
